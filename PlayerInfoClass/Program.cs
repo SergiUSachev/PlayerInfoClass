6a426ddb99cf4044982b5016283b882d073f8167
Сергей Усачев <usachevplayer@mail.ru>
2023-01-04 01:17:10 +0200

    Добавьте файлы проекта.

---
+++ PlayerInfoClass/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace PlayerInfoClass
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			PlayerInfo playerinfo = new PlayerInfo("Sergey", "Ork", 24, "Helicopter");
+			playerinfo.ShowPlayerInfo();
+		}
+	}
+	public class PlayerInfo
+	{
+		public string Name;
+		public string Race;
+		public int Age;
+		public string Gender;
+
+		public PlayerInfo(string name, string race, int age, string gender)
+		{
+			Name=name;
+			Race=race;
+			Age=age;
+			Gender=gender;
+		}
+
+		public void ShowPlayerInfo()
+		{
+			Console.WriteLine($"Информация об игроке:\nИмя: {Name} \nРаса: {Race} \nВозраст: {Age} \nПол: {Gender}");
+		}
+	}
+}
